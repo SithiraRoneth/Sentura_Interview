a4cea22cbe01b455dac9ca638e43c406ccc41dc6
USER <sithiraroneth@gmail.com>
2025-04-04 09:32:47 +0530

    add model mapper and okHttpClient

--- src/main/java/lk/ijse/sentura_interview_usercrud/SenturaInterviewUserCrudApplication.java
+++ src/main/java/lk/ijse/sentura_interview_usercrud/SenturaInterviewUserCrudApplication.java
@@ -1,7 +1,10 @@
 package lk.ijse.sentura_interview_usercrud;
 
+import okhttp3.OkHttpClient;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SenturaInterviewUserCrudApplication {
@@ -9,5 +12,13 @@ public class SenturaInterviewUserCrudApplication {
     public static void main(String[] args) {
         SpringApplication.run(SenturaInterviewUserCrudApplication.class, args);
     }
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient.Builder().build();
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
