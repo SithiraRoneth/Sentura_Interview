63c97af46301104cde4923b6807db0a6ea4024b0
USER <sithiraroneth@gmail.com>
2025-04-04 09:26:52 +0530

    add dtos

---
+++ src/main/java/lk/ijse/sentura_interview_usercrud/dto/DirectoryDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.sentura_interview_usercrud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DirectoryDTO {
+    private Integer id;
+    private String name;
+
+}
--- src/main/java/lk/ijse/sentura_interview_usercrud/dto/UserDTO.java
+++ src/main/java/lk/ijse/sentura_interview_usercrud/dto/UserDTO.java
@@ -1,9 +1,38 @@
-/* Created By Sithira Roneth
- * Date :4/4/25
- * Time :09:11
- * Project Name :Sentura_interview_UserCRUD
- * */
 package lk.ijse.sentura_interview_usercrud.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDTO {
+    private Integer id;
+    private String uid;
+    private String display_name;
+    private String avatar_url;
+    private String email;
+    private String given_name;
+    private String middle_name;
+    private String name;
+    private String family_name;
+    private String nickname;
+    private String phone_number;
+    private String comment;
+    private PictureDTO picture;
+    private Map<String, Object> metadata;
+    private List<String> tags;
+    private String presence;
+    private Integer directory_id;
+    private DirectoryDTO directory;
+    private String created_at;
+    private String updated_at;
+    private Boolean is_bot;
+    private Boolean is_suspended;
+    private Boolean is_trashed;
+
 }
